3fdd7fe4719da9e33ee530a783b13d719e96b22a
Polikkox <damian.targosz11@gmail.com>
2019-02-19 13:28:53 +0100

    Add new socket config

--- src/main/java/com/example/OnlineChatApp/Configuration/WebSocketConfiguration.java
+++ src/main/java/com/example/OnlineChatApp/Configuration/WebSocketConfiguration.java
@@ -1,17 +1,23 @@
 package com.example.OnlineChatApp.Configuration;
 
-import com.example.OnlineChatApp.Handler.WebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfiguration implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(new WebSocketHandler(), "/socket").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/subscribe-clients");
+        registry.setApplicationDestinationPrefixes("/jsa");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/js-socket-endpoint").withSockJS();
     }
 }
