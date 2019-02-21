c301defcbc89b6348b8c307909ef9b0d35c5d36a
Polikkox <damian.targosz11@gmail.com>
2019-02-21 16:45:09 +0100

    Add js to deal with front

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,78 @@
+
+var stompClient = null;
+var name = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#room").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/stomp-endpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/subscription/room', function (message) {
+            showGreeting(JSON.parse(message.body));
+        });
+        stompClient.subscribe('/welcome/onlineUsers', function (message) {
+            handleOnlineUsers(JSON.parse(message.body));
+        });
+        stompClient.send("/backend-point/getUsers", {});
+
+    });
+}
+
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+
+    stompClient.send("/backend-point/chat", {}, JSON.stringify({'message': $("#message").val()}));
+    $("#message").val("");
+}
+
+function showGreeting(message) {
+    console.log("herre");
+    console.log(message);
+    console.log("herre");
+    $("#room").append("<tr><td>" + message.from + "</td> <td>" + message.message +"</td></tr>");
+}
+
+function handleOnlineUsers(message){
+    $("#online tr").remove();
+    console.log("handle online users");
+    var users = message.onlineUsers;
+    console.log(users);
+    console.log("/handle online users");
+    for(var i in users){
+        $("#online").append("<tr><td>" + users[i]  + "</td></tr>");
+    }
+
+}
+
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendMessage(); });
+
+
+});
