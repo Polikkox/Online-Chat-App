e09ce3b12800b7fc2504782f45fe34f31c117a7a
Polikkox <damian.targosz11@gmail.com>
2019-02-22 18:38:42 +0100

    Create messenger class to push messages

---
+++ src/main/java/com/example/OnlineChatApp/Service/Messenger.java
@@ -0,0 +1,31 @@
+package com.example.OnlineChatApp.Service;
+
+import com.example.OnlineChatApp.Model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class Messenger {
+
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    public Messenger(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    public void pushInfoImpl(String url, Message message) {
+        if (simpMessagingTemplate != null) {
+            simpMessagingTemplate.convertAndSend(url, message);
+        }
+    }
+
+    public void pushInfoImpl(String url, Map<String, String> message) {
+        if (simpMessagingTemplate != null) {
+            simpMessagingTemplate.convertAndSend(url, message);
+        }
+    }
+}
