4e8bcfe3d9e7382e64302bb8dcc38fbdef69e5ee
Polikkox <damian.targosz11@gmail.com>
2019-02-22 18:37:21 +0100

    Move method send online users to UserStateController Move methods related with event listeners to EventListener Class

--- src/main/java/com/example/OnlineChatApp/Controller/WebController.java
+++ src/main/java/com/example/OnlineChatApp/Controller/WebController.java
@@ -1,54 +1,21 @@
 package com.example.OnlineChatApp.Controller;
 
 import com.example.OnlineChatApp.Model.Message;
-import com.example.OnlineChatApp.Service.HandleLogInUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import java.security.Principal;
-
 
 @Controller
 public class WebController {
 
-	private HandleLogInUser handleLogInUser;
-
 	@MessageMapping("/chat")
 	@SendTo("/subscription/room")
-	public Message greeting(Message message, Principal principal) throws Exception {
-		Message newMessage = new Message(message.getMessage(), principal.getName());
-	    return newMessage;
-	}
-
-	@MessageMapping("/getUsers")
-	@SendTo("/welcome/onlineUsers")
-	public Message sendOnlineUsers(Principal principal){
-		this.handleLogInUser.addUserToList(principal.getName());
-		Message messageNew = new Message("dfdf","sad");
-		messageNew.setOnlineUsers(handleLogInUser.getUsersList());
-		return  messageNew;
-	}
-
-	@EventListener
-	public void onSocketConnected(SessionConnectedEvent event) {
-		StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
-		System.out.println("[Connected] " + sha.getSessionId());
-	}
-
-	@EventListener
-	public void onSocketDisconnected(SessionDisconnectEvent event) {
-		StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
-		System.out.println("[Disonnected] " + sha.getSessionId());
+	public Message pushMessage(Message message, Principal principal) throws Exception {
+		return new Message(message.getMessage(), principal.getName());
 	}
 
 
-	@Autowired
-	public void setHandleLogInUser(HandleLogInUser handleLogInUser) {
-		this.handleLogInUser = handleLogInUser;
-	}
+
+
 }
