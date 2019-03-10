ee066e85c1370aa0a2ac26499225ac0d895c1fd8
Polikkox <damian.targosz11@gmail.com>
2019-03-10 20:53:15 +0100

    Add class Initial Connect Controller to handle operation related with login and creating account

---
+++ src/main/java/com/example/OnlineChatApp/Controller/InitialConnectController.java
@@ -0,0 +1,40 @@
+package com.example.OnlineChatApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/app")
+public class InitialConnectController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGet(){
+        System.out.println("login get");
+        return "/login";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPost(){
+        System.out.println("login post");
+        return "/login";
+    }
+
+
+    @RequestMapping("/messenger")
+    public String mainPage(){
+        System.out.println("messenger");
+        return "/index";
+    }
+
+    @RequestMapping(value = "/sign-up", method = RequestMethod.GET)
+    public String signUpGet(){
+        System.out.println("sign up get");
+        return "/sign-up";
+    }
+
+    @RequestMapping(value = "/sign-up", method = RequestMethod.POST)
+    public String signUpPost(){
+        System.out.println("sign up post");
+//        return "/index";
+        return "redirect:/app/messenger";
+    }
+}
