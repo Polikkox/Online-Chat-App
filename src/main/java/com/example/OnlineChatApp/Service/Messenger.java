08a9364dad72723a093f1e63af9ff3b533fccb72
Polikkox <damian.targosz11@gmail.com>
2019-03-17 00:34:26 +0100

    Add method to send Json throw messenger class

--- src/main/java/com/example/OnlineChatApp/Service/Messenger.java
+++ src/main/java/com/example/OnlineChatApp/Service/Messenger.java
@@ -1,20 +1,24 @@
 package com.example.OnlineChatApp.Service;
 
+import com.example.OnlineChatApp.Model.ChatArchive;
 import com.example.OnlineChatApp.Model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class Messenger {
 
     private SimpMessagingTemplate simpMessagingTemplate;
+    private ArrayToJsonConverter arrayToJsonConverter;
 
     @Autowired
-    public Messenger(SimpMessagingTemplate simpMessagingTemplate) {
+    public Messenger(SimpMessagingTemplate simpMessagingTemplate, ArrayToJsonConverter arrayToJsonConverter) {
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.arrayToJsonConverter = arrayToJsonConverter;
     }
 
     public void pushInfoImpl(String url, Message message) {
@@ -32,6 +36,15 @@ public class Messenger {
     public void pushInfoImpl(String url, String message) {
         if (simpMessagingTemplate != null) {
             simpMessagingTemplate.convertAndSend(url, message);
+        }
+    }
+    public void pushInfoImpl(String url, List<ChatArchive> message) {
+        if (simpMessagingTemplate != null) {
+            if(message == null){
+                simpMessagingTemplate.convertAndSend(url, "null");
+                return;
+            }
+            simpMessagingTemplate.convertAndSend(url, this.arrayToJsonConverter.convertArrayToJson(message));
         }
     }
 
